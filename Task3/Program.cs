e189ecef31ebeff92401d41b6c141c291a9eb011
calebtetteh <calebtetteh65@gmail.com>
2025-08-07 21:35:06 +0000

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Move();
+        Bicycle bicycle = new Bicycle();
+        bicycle.Move();
+    }
+
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The car is moving.");
+        }
+    }
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The bicycle is moving.");
+        }
+    }
+}
