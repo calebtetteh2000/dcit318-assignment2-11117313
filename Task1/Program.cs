0d9c64aaff3874b33f1d0213420f231f6adcc309
calebtetteh <calebtetteh65@gmail.com>
2025-08-07 20:27:57 +0000

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void makeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+class Dog : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+class Cat : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        genericAnimal.makeSound();
+        Animal dog = new Dog();
+        dog.makeSound();
+        Animal cat = new Cat();
+        cat.makeSound();
+
+    }
+}
